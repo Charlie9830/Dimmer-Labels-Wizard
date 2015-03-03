2668a59640b8e90219e31abf04991793ead42ab3
Charlie9830 <charlie.hall@ozemail.com.au>
2015-03-03 22:21:47 +0800

    Initial Commit Git Test

---
+++ Dimmer Labels Wizard/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dimmer_Labels_Wizard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Test if Git is Working as it fucking should.
+            Console.WriteLine("Git Test");
+        }
+    }
+}
