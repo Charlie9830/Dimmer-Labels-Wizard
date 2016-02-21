b6e2c7189e87eb624c06abfbc6b36a40f3a92255
charlie9830 <charlie.hall@ozemail.com.au>
2016-02-21 16:19:55 +1100

    Amended

---
+++ Dimmer Labels Wizard WPF/LabelColorManagerViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Dimmer_Labels_Wizard_WPF
+{
+    public class LabelColorManagerViewModel : ViewModelBase
+    {
+        public LabelColorManagerViewModel()
+        {
+            _Units = CollectionViewSource.GetDefaultView(Globals.DimmerDistroUnits);
+
+            
+        }
+
+        #region Binding Source Properties
+
+        protected ICollectionView _Units;
+
+        public ICollectionView Units
+        {
+            get { return _Units; }
+        }
+
+        public LabelField[] LabelFields
+        {
+            get
+            {
+                return new LabelField[] {LabelField.ChannelNumber, LabelField.InstrumentName, LabelField.Position,
+                LabelField.MulticoreName, LabelField.UserField1, LabelField.UserField2,
+                    LabelField.UserField3, LabelField.UserField4};
+            }
+        }
+
+
+        protected LabelField _SelectedLabelField;
+
+        public LabelField SelectedLabelField
+        {
+            get { return _SelectedLabelField; }
+            set
+            {
+                if (_SelectedLabelField != value)
+                {
+                    _SelectedLabelField = value;
+
+                    // Notify.
+                    OnPropertyChanged(nameof(SelectedLabelField));
+                }
+            }
+        }
+        #endregion
+
+        #region Methods
+
+        
+        #endregion
+    }
+}
---
+++ Dimmer Labels Wizard WPF/UnitGroup.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Dimmer_Labels_Wizard_WPF
+{
+    /// <summary>
+    /// Provides Grouping functionality to LabelColorManager.
+    /// </summary>
+    public class UnitGroup : ViewModelBase
+    {
+        #region Binding Source Properties
+
+        protected string _Name;
+
+        public string Name
+        {
+            get { return _Name; }
+            set
+            {
+                if (_Name != value)
+                {
+                    _Name = value;
+
+                    // Notify.
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+
+        protected IEnumerable<DimmerDistroUnit> _Units;
+
+        public IEnumerable<DimmerDistroUnit> Units
+        {
+            get { return _Units; }
+            set
+            {
+                if (_Units != value)
+                {
+                    _Units = value;
+
+                    // Notify.
+                    OnPropertyChanged(nameof(Units));
+                }
+            }
+        }
+
+
+        protected Color _Color;
+
+        public Color Color
+        {
+            get { return _Color; }
+            set
+            {
+                if (_Color != value)
+                {
+                    _Color = value;
+
+                    // Notify.
+                    OnPropertyChanged(nameof(Color));
+                }
+            }
+        }
+
+        public Brush DisplayedBrush
+        {
+            get
+            {
+                return GetDisplayedBrush();
+            }
+        }
+        #endregion
+
+        #region Methods
+        protected IEnumerable<Color> GetColorRange()
+        {
+            var colorRange = new List<Color>();
+
+            foreach (var element in Units)
+            {
+                if (colorRange.Contains(element.LabelColor) == false)
+                {
+                    colorRange.Add(element.LabelColor);
+                }
+            }
+
+            return colorRange;
+        }
+
+        protected Brush GetDisplayedBrush()
+        {
+            var colorRange = GetColorRange();
+
+            if (colorRange.Count() > 0)
+            {
+                return new SolidColorBrush(colorRange.First());
+            }
+
+            else
+            {
+                return new SolidColorBrush(Colors.White);
+            }
+        }
+
+        #endregion
+    }
+}
